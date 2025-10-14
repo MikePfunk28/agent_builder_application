64627e5c9dbc2da56a0b0c64d908e2c885b52482
MikePfunk28 <mike.pfundt@gmail.com>
2025-10-14 15:39:29 -0400

    auth config updated.
    Added github to oauth providers

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,16 @@
 export default {
   providers: [
+    // This is the default provider for Convex email/passwordless auth
     {
-      domain: process.env.SITE_URL,
+      domain: process.env.CONVEX_SITE_URL,
       applicationID: "convex",
+    },
+    // Add this provider for GitHub OAuth
+    {
+      domain: "https://github.com",
+      applicationID: "github",
+      clientID: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     },
   ],
 };
